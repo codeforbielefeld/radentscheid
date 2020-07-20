9e0a6c81f4393556a1f3b69234f4555e9b9b59f9
Sascha Koch <sascha.koch@web.de>
2020-07-20 13:46:52 +0200

    Initial commit for subproject unfallkarte

---
+++ radunfallkarte/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      Diese Webseite zeigt die Unf&auml;lle in Bielefeld und Umgebung, bei denen Fahrradfahrer involviert sind.
+    </v-flex>
+  </v-layout>
+</template>
---
+++ radunfallkarte/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="map-wrap" style="height: 80vh">
+    <no-ssr>
+      <l-map :zoom=13 :center="[52.029, 8.533]">
+        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+        <l-marker v-for="casualty in casualties" :key="casualty.id" :lat-lng="[casualty.lat, casualty.lng]">
+          <l-popup>
+            <div @click="innerClick">
+             {{ casualty.msg }} <a href="">Details</a>
+             </div>
+           </l-popup>
+        </l-marker>
+      </l-map>
+    </no-ssr>
+  </div>
+</template>
+<script>
+import cfile from './json/casualties_test.json'
+export default {
+  data () {
+    return {
+      casualties: cfile
+    }
+  },
+  methods: {
+    innerClick () {
+      alert('You clicked here')
+    }
+  }
+}
+</script>
