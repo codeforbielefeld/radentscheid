4c7655975a5b8f8c579b28e625bd8c7f4cc47cae
Sascha Koch <sascha.koch@web.de>
2020-08-02 14:08:19 +0200

    make the linter happy

--- radunfallkarte/pages/index.vue
+++ radunfallkarte/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="map-wrap" style="height: 80vh">
     <no-ssr>
-      <l-map :zoom=13 :center="[52.029, 8.533]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-map :zoom="13" :center="[52.029, 8.533]">
+        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
         <l-marker v-for="casualty in casualties" :key="casualty.id" :lat-lng="[casualty.lat, casualty.lng]">
           <l-popup>
             <div @click="innerClick">
-             {{ casualty.msg }} <a href="">Details</a>
-             </div>
-           </l-popup>
+              {{ casualty.msg }} <a href="">Details</a>
+            </div>
+          </l-popup>
         </l-marker>
       </l-map>
     </no-ssr>
@@ -17,6 +17,8 @@
 <script>
 import cfile from './json/atlas_bi_2019.json'
 export default {
+  components: {
+  },
   data () {
     return {
       casualties: cfile
