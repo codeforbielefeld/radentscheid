5aaef10d768974a4dedde4644c10046f2da44f80
Beat Grabe <beatgrabe@gmail.com>
2020-10-16 12:09:12 +0200

    Prepare page for area comparison

---
+++ radunfallkarte/pages/area-comparison.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="area-comparison">
+    :D
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.area-comparison {
+  display: flex;
+}
+</style>
