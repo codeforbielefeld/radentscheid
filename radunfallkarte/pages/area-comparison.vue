aa8e0cdd62e7310cc6db0b71145bcc171a476ffe
Beat Grabe <beatgrabe@gmail.com>
2020-10-16 12:23:07 +0200

    Add map and layers

--- radunfallkarte/pages/area-comparison.vue
+++ radunfallkarte/pages/area-comparison.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="area-comparison">
-    :D
+    <no-ssr>
+      <l-map v-bind="{ zoom, center }">
+        <l-control-layers />
+        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <l-geo-json v-bind="{ geojson }" />
+      </l-map>
+    </no-ssr>
   </div>
 </template>
 
 <script>
-export default {
+import flurstueckeSubset from './json/flurstuecke_wgs84_small.json'
 
+// eslint-disable-next-line
+import flurstueckeAll from './json/flurstuecke_wgs84.json'
+
+export default {
+  name: 'AreaComparison',
+
+  data: () => ({
+    center: [52.029, 8.533],
+    geojson: flurstueckeSubset,
+    zoom: 12
+  })
 }
 </script>
 
 <style>
 .area-comparison {
-  display: flex;
+  height: 80vh;
 }
 </style>
