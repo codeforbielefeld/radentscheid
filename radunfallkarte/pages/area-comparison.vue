29df4992326c0021312cf6d6a8f0f8bd7431a2a9
Beat Grabe <beatgrabe@gmail.com>
2020-10-16 12:37:34 +0200

    Add wms layer

--- radunfallkarte/pages/area-comparison.vue
+++ radunfallkarte/pages/area-comparison.vue
@@ -6,11 +6,22 @@
           <l-control-layers />
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
           <l-geo-json v-if="geojsonEnabled" v-bind="{ geojson }" />
+          <template v-if="wmsEnabled">
+            <l-lwms-tile-layer
+              v-for="layer in wms.layers"
+              :key="layer.name"
+              :base-url="wms.baseUrl"
+              :format="layer.format"
+              :layers="layer.layers"
+              :name="layer.name"
+            />
+          </template>
         </l-map>
       </div>
 
       <div class="area-comparison__controls">
         <v-checkbox v-model="geojsonEnabled" label="GeoJSON" />
+        <v-checkbox v-model="wmsEnabled" label="WMS (need to zoom)" />
       </div>
     </no-ssr>
   </div>
@@ -22,6 +33,17 @@ import flurstueckeSubset from './json/flurstuecke_wgs84_small.json'
 // eslint-disable-next-line
 import flurstueckeAll from './json/flurstuecke_wgs84.json'
 
+const WMS_BIELEFELD = {
+  baseUrl: 'http://www.bielefeld01.de/md/WMS/alkis_light/03',
+  layers: [
+    {
+      format: 'image/png',
+      layers: 'alkis_flst_pl',
+      name: 'Flurstuecke'
+    }
+  ]
+}
+
 export default {
   name: 'AreaComparison',
 
@@ -29,6 +51,8 @@ export default {
     center: [52.029, 8.533],
     geojson: flurstueckeSubset,
     geojsonEnabled: true,
+    wms: WMS_BIELEFELD,
+    wmsEnabled: false,
     zoom: 12
   })
 }
